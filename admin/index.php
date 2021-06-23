640d7eed737abca6d8d543d515d53611c132f61b
damiscloudaku <damis@cloudaku.com>
2021-06-23 08:47:51 +0700

    edit judul

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@ $a = mysql_query("select * from barang");
 ?>
 
 <div class="col-md-10">
-	<h3>Selamat datang</h3>	
+	<h3>Selamat Datang</h3>	
     <h3>Aplikasi Stok Barang</h3>
     <!-- <h3>WWW.MALASNGODING.COM</h3> -->
 </div>
